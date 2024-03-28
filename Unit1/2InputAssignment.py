5122b91e2d0a1d6af9ddcdf213fb72d545c7a930
Zach-Rife <26zacharyrife@usd266.com>
2024-03-28 12:59:03 -0500

    assignment 2

--- Unit1/2InputAssignment.py
+++ Unit1/2InputAssignment.py
@@ -1,15 +1,31 @@
 print("########## 1.2.1 ##########")
-
+name = input("What is your name")
+print(name)
+print(name)
 print("########## 1.2.2 ##########")
+name = input("What is your name?")
+print("!"+ name + "!" + name + "!" )
 
 print("########## 1.2.3 ##########")
-
+firstname = input("Enter your first name") 
+lastname = input("Enter your last name")
+streetaddress = input("Enter your street address")
+addresscity = input("Enter your address city")
+addressstate = input("Enter your address state")
+zipcode = input("Enter your zip code")
+print( firstname + " " +  lastname)
+print(streetaddress)
+print(addresscity + " " + addressstate + " " + zipcode)
 print("########## 1.2.4 ##########")
 
 # Fix the code
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-print(part + part + part)
+part1 = input("The 1st part: ")
+part2 = input("The 2nd part: ")
+part3 = input("The 3rd part: ")
+print(part1 + " " +  part2 + " " + part3)
 
 print("########## 1.2.5 ##########")
+name = input("Please type in a name")
+year = input("please type in a year")
+
+print(name + "is a valiant knight, born in the year" + " " +  year + "." + "One morning" + " " + name + " " + "woke up to an awful racket: a dragon was approaching the village. Only" + " " + name +" " + "could save the village's residents.")
