fbf5aef1a42fc1e2fe642439a858fd09d179a415
Zach-Rife <26zacharyrife@usd266.com>
2024-05-10 12:18:58 -0500

    done

--- Unit4/1PrintingFunctionsAssignment.py
+++ Unit4/1PrintingFunctionsAssignment.py
@@ -1,19 +1,82 @@
 # ========== 4.1.1 ==========
-
+def line(times, text):
+    if text == "":
+        print("*" * times)
+    else:
+        print(text[0] * times)
+# times = int(input("number"))
+# text = input("Text")
+# line(times, text)
 
 # ========== 4.1.2 ==========
+# def box_of_hashes(height):
+#     count = 0
+#     while True:
+#         line(10, "#")
+#         count += 1
+#         if count >= height:
+#             break
+# box_of_hashes(5)
 
+# # ========== 4.1.3 ==========
+# def square_of_hashes(height):
+#     count = 0
+#     while True:
+#         line(times, "#")
+#         count += 1
+#         if count >= height:
+#             break
+# square_of_hashes(5)
 
-# ========== 4.1.3 ==========
-
-
-# ========== 4.1.4 ==========
-
+# # ========== 4.1.4 ==========
+# def square(times, text):
+#     count = 0
+#     while True:
+#         line(times, text)
+#         count += 1
+#         if count >= times:
+#             break
+# square(5, "*")
 
 # ========== 4.1.5 ==========
-
+# def triangle(times):
+#     count = 0
+#     num = 1
+#     while True:
+#         line(num, "#")
+#         count += 1
+#         num += 1
+#         if count >= times:
+#             break
+# triangle(6)
 
 # ========== 4.1.6 ==========
-
+# def shape(num1, let1, num2, let2):
+#     count = 1
+#     while count < num1 + 1:
+#         line(count, let1)
+#         count += 1
+#     count = 0
+#     while count < num2:
+#         line(num1, let2)
+#         count+=1
+# shape(5, "x", 3, "*")
 
 # ========== 4.1.7 ==========
+def spruce(width):
+    count = 0
+    num = 1
+    space = width
+    while count < width:
+        print(" " * (space-1), "*" * num)
+        num += 2
+        count += 1
+        space += 1
+        print()
+
+
+        
+        
+  
+        
+spruce(3)
--- Unit4/2ReturningFunctionsDemo.py
+++ Unit4/2ReturningFunctionsDemo.py
@@ -1,12 +1,82 @@
 # ========== 4.2.1 ==========
-
+# def greatest_number(n1, n2, n3):
+#     if n1 > n2 and n1 > n3:
+#         return n1
+#     elif n2 > n3:
+#         return n2
+#     else:
+#         return n3
+# n1 = int(input("number:"))
+# n2 = int(input("number:"))
+# n3 = int(input("number:"))
+# greatest = greatest_number(n1, n2, n3)
+# print("The greatest number is", greatest)
 
 
 # ========== 4.2.2 ==========
-
+def same_char(word, n1, n2):
+    if word[n1] == word[n2]:
+        return "True"
+    else:
+        return "false"
+word = input("Please type in a string")
+n1 = int(input("Please type in a number"))
+n2 = int(input("Please type in a number"))
+sameword = same_char(word, n1, n2)
+print(sameword)
 
 
 # ========== 4.2.3 ==========
+sentence = "It was a dark and stormy python"
+def first_word(sentence):
+    index = 0
+    sent = ""
+    while True:
+        if sentence[index] == " ":
+            break
+        else:
+            sent = sent + sentence[index]
+            index = index + 1
+    return(sent)
+first = first_word(sentence)
+print(first)
+
+
+sentence = "It was a dark and stormy python"
+def second_word(sentence):
+    first = len(first_word(sentence))
+    index = 0
+    sent = ""
+    while True:
+        if sentence[index] == " ":
+            if first == index:
+                index = index + 1
+                sent = " "
+            else:
+                break
+        else:
+            sent = sent + sentence[index]
+            index = index + 1
+    return(sent)
+second = second_word(sentence)
+print(second)
+
+
+
+
+sentence = "It was a dark and stormy python"
+def first_word(sentence):
+    index = len(sentence)- 1
+    sent = ""
+    while True:
+        if sentence[index] == " ":
+            break
+        else:
+            sent = sentence[index] + sent
+            index = index -1
+    return(sent)
+first = first_word(sentence)
+print(first)
 
 
 
